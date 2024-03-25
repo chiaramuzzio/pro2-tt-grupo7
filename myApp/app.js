bd9b80317f11d7c561c3c56d66086062b2486d7c
chiaramuzzio <cmuzzio@udesa.edu.ar>
2024-03-25 17:06:46 -0300

    rutas, cambio links y partials

--- myApp/app.js
+++ myApp/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var profileRouter = require('./routes/profile');
+var productRouter = require('./routes/product');
+var searchRouter = require('./routes/search');
 
 var app = express();
 
@@ -21,6 +24,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/profile', profileRouter);
+app.use('/product', productRouter);
+app.use('/search-results', searchRouter);
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
