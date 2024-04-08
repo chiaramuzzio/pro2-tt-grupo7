83318809c5a3de974b5687f6325c7d5fe01120ea
GregoBonardi <gbonardi@udesa.edu.ar>
2024-04-08 17:40:27 -0300

    creacion SQL, tabla usuarios y productos, Foreign key

---
+++ myApp/db/data.sql
@@ -0,0 +1,32 @@
+create schema listaUsuarios;
+use listaUsuarios;
+
+CREATE TABLE usuarios (
+/* 	nombreColumna 		tipoDato 		Restricciones */
+    id 					INT 			UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    mail 				VARCHAR(250) 	NOT NULL,
+    usuario 			VARCHAR(250) 	NOT NULL,
+    contrasenia 		VARCHAR(250) 	NOT NULL,
+    fechaNacimiento 	DATE 			NOT NULL,
+    numeroDocumento 	INT 			NOT NULL,
+    foto 				VARCHAR(250) 	NOT NULL,
+    createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
+	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt			timestamp		null
+);
+CREATE TABLE productos (
+/* 	nombreColumna 		tipoDato 		Restricciones */
+    id 					INT 			UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    cliente_id			INT				UNSIGNED,
+    nombreProd 			VARCHAR(250) 	NOT NULL,
+    imagenProd 			VARCHAR(250) 	NOT NULL,
+    descripcion 		VARCHAR(250) 	NOT NULL,
+    createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
+	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt			timestamp		null,
+    
+    FOREIGN KEY (cliente_id) REFERENCES usuarios(id)
+
+)
+
+
