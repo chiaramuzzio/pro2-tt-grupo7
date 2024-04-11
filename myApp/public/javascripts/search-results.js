fa98dd69b1bf73ea98925cb474811b9d52e32893
GregoBonardi <gbonardi@udesa.edu.ar>
2024-04-11 15:03:36 -0300

    search results

---
+++ myApp/public/javascripts/search-results.js
@@ -0,0 +1,10 @@
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let resulBusqueda = queryStringObj.get("search");
+
+
+let textoBusqueda = document.querySelector("#texto-busqueda");
+textoBusqueda.innerText =  `Resultados de búsqueda: ${resulBusqueda}`;
+
+
+console.log("hola"); 
