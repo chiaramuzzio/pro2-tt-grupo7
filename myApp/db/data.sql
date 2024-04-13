8ce18431ec8e2bbecd6c90fc299e9ca631761a19
fruizguinazu <fruizguinazu@udesa.edu.ar>
2024-04-13 17:51:28 -0300

    arreglo+inserts comentarios SQL

--- myApp/db/data.sql
+++ myApp/db/data.sql
@@ -23,7 +23,7 @@ CREATE TABLE productos (
 /* 	Nombre de Columna   Tipo de Dato 	Restricciones */
 
     id 					INT 			UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    cliente_id			INT				UNSIGNED,
+    clienteId			INT				UNSIGNED,
     nombreProd 			VARCHAR(250) 	NOT NULL,
     imagenProd 			VARCHAR(250) 	NOT NULL,
     descripcion 		VARCHAR(250) 	NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE productos (
 	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deletedAt			timestamp		null,
     
-    FOREIGN KEY (cliente_id) REFERENCES usuarios(id)
+    FOREIGN KEY (clienteId) REFERENCES usuarios(id)
 );
 
 CREATE TABLE comentarios (
@@ -39,15 +39,15 @@ CREATE TABLE comentarios (
 /* 	Nombre de Columna 	Tipo de Dato 	Restricciones */
 
     id 					INT 			UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    producto_id			INT				UNSIGNED,
-    cliente_id			INT				UNSIGNED,
+    productoId			INT				UNSIGNED,
+    clienteId			INT				UNSIGNED,
     comentario 			VARCHAR(250) 	NOT NULL,
     createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
 	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deletedAt			timestamp		null,
 
-    FOREIGN KEY (cliente_id) REFERENCES usuarios(id),
-    FOREIGN KEY (producto_id) REFERENCES productos(id)
+    FOREIGN KEY (clienteId) REFERENCES usuarios(id),
+    FOREIGN KEY (productoId) REFERENCES productos(id)
 );
 
     /*SQL usuarios*/
@@ -69,43 +69,123 @@ values (default, 'felitrazzo@gmail.com', 'felicitastrazzo', 'carlitos10', '1964/
 
     /*SQL productos*/
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro1', 'libro1.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 4, 'libro1', 'libro1.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro2', 'libro2.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 3, 'libro2', 'libro2.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro3', 'libro3.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 2, 'libro3', 'libro3.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro4', 'libro4.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 4, 'libro4', 'libro4.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro5', 'libro5.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 1, 'libro5', 'libro5.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro6', 'libro6.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 1, 'libro6', 'libro6.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro7', 'libro7.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 3, 'libro7', 'libro7.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro8', 'libro8.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 5, 'libro8', 'libro8.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro9', 'libro9.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 3, 'libro9', 'libro9.png', 'lorem ipsum');
 
-insert into productos (id, cliente_id, nombreProd, imagenProd, descripcion)
-values (default, cliente_id, 'libro10', 'libro10.png', 'lorem ipsum');
+insert into productos (id, clienteId, nombreProd, imagenProd, descripcion)
+values (default, 1, 'libro10', 'libro10.png', 'lorem ipsum');
 
     /*SQL comentarios*/
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 1, 5, 'comentario 1');
 
-insert into comentarios (id, producto_id, cliente_id, comentario)
-values (default, producto_id, cliente_id, 'comentario 1');
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 1, 1, 'comentario 2');
 
-insert into comentarios (id, producto_id, cliente_id, comentario)
-values (default, producto_id, cliente_id, 'comentario 2');
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 1, 3, 'comentario 3');
 
-insert into comentarios (id, producto_id, cliente_id, comentario)
-values (default, producto_id, cliente_id, 'comentario 3');
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 2, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 2, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 2, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 3, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 3, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 3, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 4, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 4, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 4, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 5, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 5, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 5, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 6, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 6, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 6, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 7, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 7, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 7, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 8, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 8, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 8, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 9, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 9, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 9, 3, 'comentario 3');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 10, 5, 'comentario 1');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 10, 1, 'comentario 2');
+
+insert into comentarios (id, productoId, clienteId, comentario)
+values (default, 10, 3, 'comentario 3');
