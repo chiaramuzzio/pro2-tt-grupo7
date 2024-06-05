110c07642f79ddef5488a8710db1a707697b0dba
GregoBonardi <gbonardi@udesa.edu.ar>
2024-06-05 19:14:58 -0300

    modelo comentario

--- myApp/database/models/Comentario.js
+++ myApp/database/models/Comentario.js
@@ -34,5 +34,19 @@ module.exports = function (sequelize, dataTypes ) {
     
     let Comentario = sequelize.define(alias, cols, config);
   
+    Comentario.associate = function(models) {
+        
+        Comentario.belongsTo(models.Usuario, {
+            as: 'usuario',
+            foreignKey: 'clienteId'
+        });
+    
+    
+        Comentario.belongsTo(models.Producto, {
+            as: 'producto',
+            foreignKey: 'productoId'
+        });
+    }
+
     return Comentario;
 }
